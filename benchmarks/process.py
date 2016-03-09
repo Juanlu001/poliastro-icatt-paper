66418b0423a31accd23afed218f16a8e27f10c5e
Juan Luis Cano Rodriguez <jlcr@gmv.es>
2016-03-09 10:35:11 +0100

    Change Vallado JIT benchmark

--- benchmarks/process.py
+++ benchmarks/process.py
@@ -45,6 +45,28 @@ def plot_benchmark(df, **fig_kwargs):
     return fig, (ax1, ax2)
 
 
+def plot_benchmark_vallado(df, **fig_kwargs):
+    fig, ax1 = plt.subplots(1, **fig_kwargs)
+
+    sns.boxplot(x="time", y="test", hue="warmup", data=df[df["jit"] == True],
+                orient="h", showfliers=False, palette="YlOrRd_d",
+                ax=ax1)
+    ax1.set(xlabel='Time, JIT enabled ($\mu s$)')
+    ax1.set_xlim(6.0, 20.0)
+    ax1.legend(loc='center left', title="warmup")
+
+    ax2 = ax1.twiny()
+
+    sns.boxplot(x="time", y="test", hue="warmup", data=df[df["jit"] == False],
+                orient="h", showfliers=False, palette="PuBuGn_d",
+                ax=ax2)
+    ax2.set(xlabel='Time, JIT disabled ($\mu s$)', ylabel='Test')
+    ax2.set_xlim(880.0, 1020.0)
+    ax2.legend(loc='center right', title="warmup")
+
+    return fig, (ax1, ax2)
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -54,8 +76,10 @@ if __name__ == '__main__':
     sns.set(style="whitegrid")
     plt.rc("font", family="serif")
 
-    fig, axes = plot_benchmark(df[df["test"] == "test_lambert_single_rev_vallado"],
-                               sharex=False, figsize=(12, 3))
+    fig, axes = plot_benchmark_vallado(
+        df[df["test"] == "test_lambert_single_rev_vallado"],
+        figsize=(12, 2)
+    )
     fig.tight_layout()
     fig.savefig("benchmark_vallado.pdf")
 
