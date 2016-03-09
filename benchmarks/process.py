ec0af64531e85dc0510a682ac23cc21208680d8b
Juan Luis Cano Rodriguez <jlcr@gmv.es>
2016-03-09 11:06:20 +0100

    Add whole JIT benchmark figure

--- benchmarks/process.py
+++ benchmarks/process.py
@@ -27,24 +27,6 @@ def aggregate_data():
     return pd.DataFrame(data)
 
 
-def plot_benchmark(df, **fig_kwargs):
-    fig, (ax1, ax2) = plt.subplots(2, **fig_kwargs)
-    for ax, jit in ((ax1, False), (ax2, True)):
-        title = {False: "Benchmark, JIT disabled",
-                 True: "Benchmark, JIT enabled"}[jit]
-
-        sns.boxplot(x="time", y="test", hue="warmup", data=df[df["jit"] == jit],
-                    orient="h", showfliers=False, palette="PuBuGn_d",
-                    ax=ax)
-
-        ax.set(xlabel='Time ($\mu s$)', ylabel='Test',
-               title=title)
-
-    ax1.legend_.remove()
-
-    return fig, (ax1, ax2)
-
-
 def plot_benchmark_izzo(df, **fig_kwargs):
     fig, ax1 = plt.subplots(1, **fig_kwargs)
 
@@ -64,7 +46,7 @@ def plot_benchmark_izzo(df, **fig_kwargs):
     ax2.set_xlim(200.0, 300.0) 
     ax2.legend(loc='center right', title="warmup")
 
-    return fig, ax1
+    return fig, (ax1, ax2)
 
 
 def plot_benchmark_vallado(df, **fig_kwargs):
@@ -89,6 +71,55 @@ def plot_benchmark_vallado(df, **fig_kwargs):
     return fig, (ax1, ax2)
 
 
+def plot_benchmark_all(df, **fig_kwargs):
+    fig, (ax1, ax3) = plt.subplots(2, sharex=False, gridspec_kw={'height_ratios':[2, 3]}, **fig_kwargs)
+
+    df_vallado = df[df["test"] == "test_lambert_single_rev_vallado"]
+
+    sns.boxplot(x="time", y="test", hue="warmup",
+                data=df_vallado[df_vallado["jit"] == True],
+                orient="h", showfliers=False, palette="YlOrRd_d",
+                ax=ax1)
+    ax1.set(xlabel='Time, JIT enabled ($\mu s$)')
+    ax1.set_xlim(6.0, 20.0)
+    ax1.legend(loc='center left', title="warmup")
+
+    ax2 = ax1.twiny()
+
+    sns.boxplot(x="time", y="test", hue="warmup",
+                data=df_vallado[df_vallado["jit"] == False],
+                orient="h", showfliers=False, palette="PuBuGn_d",
+                ax=ax2)
+    ax2.set(xlabel='Time, JIT disabled ($\mu s$)', ylabel='Test')
+    ax2.set_xlim(880.0, 1020.0)
+    ax2.legend(loc='center right', title="warmup")
+
+    df_izzo = df[(
+        (df["test"] == "test_lambert_single_rev_izzo") |
+        (df["test"] == "test_lambert_multi_rev_izzo")
+    )]
+
+    sns.boxplot(x="time", y="test", hue="warmup",
+                data=df_izzo[df_izzo["jit"] == True],
+                orient="h", showfliers=False, palette="YlOrRd_d",
+                ax=ax3)
+    ax3.set(xlabel='Time, JIT enabled ($\mu s$)', ylabel='Test')
+    ax3.set_xlim(120.0, 220.0)
+    ax3.legend(loc='center left', title="warmup")
+
+    ax4 = ax3.twiny()
+
+    sns.boxplot(x="time", y="test", hue="warmup",
+                data=df_izzo[df_izzo["jit"] == False],
+                orient="h", showfliers=False, palette="PuBuGn_d",
+                ax=ax4)
+    ax4.set(xlabel='Time, JIT disabled ($\mu s$)', ylabel='Test')
+    ax4.set_xlim(200.0, 300.0)
+    ax4.legend(loc='center right', title="warmup")
+
+    return fig, (ax1, ax2, ax3, ax4)
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -111,3 +142,7 @@ if __name__ == '__main__':
     )], figsize=(12, 2.5))
     fig.tight_layout()
     fig.savefig("benchmark_izzo.pdf")
+
+    fig, axes = plot_benchmark_all(df, figsize=(12, 4))
+    fig.tight_layout()
+    fig.savefig("benchmark_all.pdf")
