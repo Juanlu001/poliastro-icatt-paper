70c750e53a32974066356c4cc6bf6e196d2de4d3
Juan Luis Cano Rodriguez <jlcr@gmv.es>
2016-03-09 16:31:39 +0100

    JIT also outer loop, 200 k

--- comparisons/performance.py
+++ comparisons/performance.py
@@ -6,6 +6,7 @@ import numpy as np
 import astropy.units as u
 
 from poliastro.bodies import Earth
+from poliastro.jit import jit
 from poliastro.iod import vallado, izzo
 
 R_BASE = 1e6
@@ -15,14 +16,19 @@ TOF_BASE = 86400.0
 N_CASES = 1000000
 
 
+@jit
 def main():
     for ii in range(N_CASES):
         k = 3.986004362330e5
-        r0 = np.random.uniform(-R_BASE, R_BASE, 3)
-        r = np.random.uniform(-R_BASE, R_BASE, 3)
+        r0 = np.zeros(3)
+        r = np.zeros(3)
+        for ii in range(3):
+            r0[ii] = np.random.uniform(-R_BASE, R_BASE)
+            r[ii] = np.random.uniform(-R_BASE, R_BASE)
+
         tof = np.random.uniform(1000.0, TOF_BASE)
 
-        solutions = tuple([sol for sol in izzo._lambert(k, r0, r, tof, 0, 35, 1e-8)])
+        sol1, = izzo._lambert(k, r0, r, tof, 0, 35, 1e-8)
 
 
 if __name__ == '__main__':
