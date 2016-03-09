53b3bbb2f994ea0feb08874c09418898b401434f
Juan Luis Cano Rodriguez <jlcr@gmv.es>
2016-03-09 16:02:04 +0100

    Cosmetic changes

--- comparisons/performance.py
+++ comparisons/performance.py
@@ -1,4 +1,5 @@
 import time
+import collections
 
 import numpy as np
 
@@ -15,20 +16,17 @@ N_CASES = 1000000
 
 
 def main():
-    k = 3.986004362330e5
-
-    t_start = time.process_time()
     for ii in range(N_CASES):
+        k = 3.986004362330e5
         r0 = np.random.uniform(-R_BASE, R_BASE, 3)
         r = np.random.uniform(-R_BASE, R_BASE, 3)
         tof = np.random.uniform(1000.0, TOF_BASE)
 
-        solutions = [sol for sol in izzo._lambert(k, r0, r, tof, 0, 35, 1e-8)]
-
-    t_end = time.process_time()
-
-    return t_end - t_start, N_CASES / (t_end - t_start)
+        solutions = tuple([sol for sol in izzo._lambert(k, r0, r, tof, 0, 35, 1e-8)])
 
 
 if __name__ == '__main__':
-    print(main())
+    t_start = time.process_time()
+    main()
+    t_end = time.process_time()
+    print((t_end - t_start), N_CASES / (t_end - t_start))
